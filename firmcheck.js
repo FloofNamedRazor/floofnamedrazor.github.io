795e64fe45ba0b773269335d5a1a682d74395b2f
FloofNamedRazor <33744544+FloofNamedRazor@users.noreply.github.com>
2018-07-25 14:44:23 -0700

    Update firmcheck.js

--- firmcheck.js
+++ firmcheck.js
@@ -1,13 +1,6 @@
-$(document).ready(function() {
-	function iOSversion() {
-		if (/iP(hone|od|ad)/.test(navigator.platform)) {
-			// supports iOS 2.0 and later: <https://bit.ly/TJjs1V>
-			var v = (navigator.appVersion).match(/OS (\d+)_(\d+)_?(\d+)?/);
-			return [parseInt(v[1], 10), parseInt(v[2], 10), parseInt(v[3] || 0, 10)];
-		}
-	}
+Use it like this:
+ver = iOSversion();
 
-	ver = iOSversion();
-
-	if (ver[0] >= 5) {
-		alert('This is running iOS '+ver);
+if (ver[0] >= 5) {
+alert('This is running iOS 5 or later.');
+}
